78fd83326d18d658ebd8b9b47ab75c6bceaf5e94
AmjedNazzal <amjednazzal@outlook.com>
2025-05-04 17:13:34 +0300

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,88 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+type Item = {
+  id: number;
+  ItemTitle: string;
+  itemDescription: string;
+};
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+type HomePageData = {
+  pageTitle: string;
+  pageSubtitle: string;
+  pageDesc: string;
+  items: Item[];
+};
 
 export default function Home() {
+  const [data, setData] = useState<HomePageData | null>(null);
+  const [openItemId, setOpenItemId] = useState<number | null>(0);
+
+  useEffect(() => {
+    axios.get("http://localhost:1337/api/main?populate=items").then((res) => {
+      console.log(res);
+      const { pageTitle, items, pageSubtitle, pageDesc } = res.data.data;
+      setData({ pageTitle, items, pageSubtitle, pageDesc });
+    });
+  }, []);
+
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main className="p-6 flex flex-col mx-auto font-sans gap-6">
+      <div className="flex flex-col gap-2 items-center">
+        <h2 className="text-[24px]">{data.pageTitle}</h2>
+        <h1 className="text-[36px] font-bold">{data.pageSubtitle}</h1>
+        <p>{data.pageDesc}</p>
+      </div>
+      <div className="grid grid-cols-2 gap-[50px] w-full justify-between px-[100px]">
+        <div className="flex w-full relative items-center">
+          <div className="w-[230px] z-[-1] h-[230px] overflow-hidden rounded-full absolute right-[15%]">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={"/mainImage.jpg"}
+              width={230}
+              height={230}
+              alt="main image"
+              className="overflow-hidden rouned-full object-cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <div className="flex w-[230px] h-[230px] rounded-full bg-red-300/70 overflow-hidden p-4 items-center">
+            {typeof openItemId === "number" && (
+              <div className="flex flex-col gap-2">
+                <p className="text-white text-[14px] font-bold">
+                  {data.items[openItemId].ItemTitle}
+                </p>
+                <p className="text-white text-[12px] leading-tight break-words">
+                  {data.items[openItemId].itemDescription}
+                </p>
+              </div>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <ul className="space-y-3">
+          {data.items.map((item, index) => (
+            <li key={index}>
+              <button
+                className={`flex w-full justify-between font-bold ${
+                  openItemId === index ? "bg-red-300" : "bg-gray-300"
+                } py-2 px-4 rounded-full`}
+                onClick={() =>
+                  setOpenItemId(openItemId === index ? null : index)
+                }
+              >
+                <p>{"<"}</p>
+                <p className={`${openItemId === index ? "text-white" : ""}`}>
+                  {item.ItemTitle}
+                </p>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
